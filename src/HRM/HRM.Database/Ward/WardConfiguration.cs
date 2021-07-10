0dbabc4a370f6b3cb85b7c8de4abc4f7652c7051
dinhhung09138 <dinhhung09138@gmail.com>
2021-07-10 20:21:49 +0700

    Database initial

---
+++ src/HRM/HRM.Database/Ward/WardConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using HRM.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Database
+{
+    public sealed class WardConfiguration : IEntityTypeConfiguration<Ward>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Ward> builder)
+        {
+            builder.ToTable(nameof(Ward), MessageConstant.SchemaName);
+            builder.HasKey(m => m.Id);
+            builder.Property(m => m.Id).ValueGeneratedOnAdd().IsRequired();
+
+            builder.Property(m => m.Name).HasMaxLength(100).IsRequired();
+
+            builder.Property(m => m.IsActive).IsRequired();
+
+        }
+    }
+}
