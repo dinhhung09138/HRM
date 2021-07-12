9fea8fc47d8f679dd38aec9350d0770ffe12c4d5
vuchungdung <vuchungdunghlym@gmail.com>
2021-07-12 10:01:41 +0700

    update code

---
+++ src/HRM/HRM.Domain/Major.cs
@@ -0,0 +1,54 @@
+﻿using DotNetCore.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Domain
+{
+    public class Major : Entity<long>
+    {
+        public Major()
+        {
+
+        }
+
+        [Column(TypeName = "nvarchar(150)")]
+        [MaxLength(150)]
+        [Required]
+        public string Name { get; set; }
+
+        [Column(TypeName = "int")]
+        [Required]
+        public int Precedence { get; set; }
+
+        [Column(TypeName = "bit")]
+        [Required]
+        public bool IsActive { get; set; }
+
+        [Column(TypeName = "int")]
+        [Required]
+        public int CreateBy { get; set; }
+
+        [Column(TypeName = "datetime")]
+        [Required]
+        public DateTime CreateDate { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime? UpdateDate { get; set; }
+
+        [Column(TypeName = "int")]
+        public int? UpdateBy { get; set; }
+
+        [Column(TypeName = "bit")]
+        [Required]
+        public bool Deleted { get; set; }
+
+        [Column(TypeName = "timestamp")]
+        [Required]
+        public byte[] RowVersion { get; set; }
+    }
+}
