0eb4fff452ea3ecca11e924ac5ff9b1c61a28e3f
dinhhung09138 <dinhhung09138@gmail.com>
2021-07-17 20:50:11 +0700

    build unitofWork class

---
+++ src/HRM/HRM.Database/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace HRM.Database
+{
+    public interface IUnitOfWork
+    {
+        Task BeginTranactionAsync();
+
+        Task RollBackAsync();
+
+        Task CommitAsync();
+
+        Task SaveChangesAsync();
+    }
+}
---
+++ src/HRM/HRM.Database/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Database
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly Context _context;
+
+        public UnitOfWork(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task BeginTranactionAsync()
+        {
+            await _context.Database.BeginTransactionAsync();
+        }
+
+        public async Task RollBackAsync()
+        {
+            await _context.Database.RollbackTransactionAsync();
+        }
+        public async Task CommitAsync()
+        {
+            await _context.Database.CommitTransactionAsync();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
