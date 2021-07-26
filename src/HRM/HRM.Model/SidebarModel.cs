ce4500d68df1ca154ad128305650fc773e79c416
hung.tran <hung.tran@saigontechnology.com>
2021-07-26 21:03:38 +0700

    Build and test sidebar component (keep update)

---
+++ src/HRM/HRM.Model/SidebarModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Model
+{
+    public class SidebarModel
+    {
+        public string Id { get; set; }
+        public string Icon { get; set; }
+        public string Title { get; set; }
+
+        public string Href { get; set; }
+
+        public List<SidebarModel> MenuItems { get; set; } = new List<SidebarModel>();
+
+        public SidebarModel()
+        {
+
+        }
+
+        public SidebarModel(string id, string title, string href, string icon)
+        {
+            Id = id;
+            Title = title;
+            Href = href;
+            Icon = icon;
+        }
+    }
+}
