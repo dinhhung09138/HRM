2958968a2b7e6d40448265653e23193fa35e21f6
hung.tran <hung.tran@saigontechnology.com>
2021-08-03 21:45:36 +0700

    Addind server project

---
+++ src/HRM/HRM.Server/Extensions/ServiceExtension.cs
@@ -0,0 +1,34 @@
+﻿using DotNetCore.EntityFrameworkCore;
+using DotNetCore.IoC;
+using HRM.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRM.Server.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static IServiceCollection UseDatabase(this IServiceCollection services)
+        {
+            var connectionString = services.GetConnectionString(nameof(Context));
+            services.AddContext<Context>(options => options.UseSqlServer(connectionString));
+            //services.AddUnitOfWork<Context>();
+            return services;
+        }
+
+        public static IServiceCollection UseServices(this IServiceCollection services)
+        {
+            services.AddScoped<Database.IUnitOfWork, UnitOfWork>();
+            return services;
+        }
+
+        public static IServiceCollection UseStaticFiles(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ src/HRM/HRM.Server/wwwroot/js/app.js
@@ -0,0 +1,25 @@
+﻿function showHideMenu() {
+	if (getWidth() >= 768) {
+		if ($('body').hasClass('sidebar-collapse')) {
+			$('body').attr('class', 'sidebar-mini pace-done sidebar-open');
+		} else {
+			$('body').attr('class', 'sidebar-mini pace-done sidebar-open sidebar-collapse');
+		}
+	} else {
+		if ($('body').hasClass('sidebar-open')) {
+			$('body').attr('class', 'sidebar-mini pace-done');
+		} else {
+			$('body').attr('class', 'sidebar-mini pace-done sidebar-open');
+		}
+	}
+}
+
+function getWidth() {
+	return Math.max(
+		document.body.scrollWidth,
+		document.documentElement.scrollWidth,
+		document.body.offsetWidth,
+		document.documentElement.offsetWidth,
+		document.documentElement.clientWidth
+	);
+}
