29485c868b8f701d02161cf5e3099487b23dbcbd
hung.tran <hung.tran@saigontechnology.com>
2021-08-03 21:47:59 +0700

    Update client project

---
+++ src/HRM/HRM.Client/Services/HttpClientService.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HRM.Model;
+
+namespace HRM.Client.Services
+{
+    public class HttpClientService : IHttpClientService
+    {
+        private readonly HttpClient _httpClient;
+
+        public HttpClientService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ResponseModel> Get(string url)
+        {
+            ResponseModel md = new ResponseModel();
+
+            return md;
+        }
+    }
+}
