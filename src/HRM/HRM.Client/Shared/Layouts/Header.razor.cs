04ad6588bf1916f47de09100d5bff302f37c61c5
hung.tran <hung.tran@saigontechnology.com>
2021-08-21 21:44:01 +0700

    Redirecto to login if user not authorize, Do Logout function

--- src/HRM/HRM.Client/Shared/Layouts/Header.razor.cs
+++ src/HRM/HRM.Client/Shared/Layouts/Header.razor.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using HRM.Client.Auth;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
@@ -10,11 +12,22 @@ namespace HRM.Client.Shared.Layouts
     public partial class Header
     {
         [Inject]
+        public NavigationManager navigationManager { get; set; }
+        [Inject]
+        public AuthenticationStateProvider authenticationStateProvide { get; set; }
+
+        [Inject]
         public IJSRuntime js { get; set; }
 
         protected async Task ShowHideSidebar()
         {
             await js.InvokeVoidAsync("showHideMenu");
+        }
+
+        protected async Task LogoutClick()
+        {
+            await ((HrmAuthenticationStateProvider)authenticationStateProvide).DoLogout();
+            navigationManager.NavigateTo("/login");
         }
     }
 }
