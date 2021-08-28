cf5078d52bc043d0878257fe0d9d121a39779457
hung.tran <hung.tran@saigontechnology.com>
2021-08-28 20:47:58 +0700

    Adding breadcrumb

---
+++ src/HRM/HRM.Model/UI/BreadcurmbModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Model.UI
+{
+    public class BreadcurmbModel
+    {
+        public string Href { get; set; } = string.Empty;
+
+        public string Title { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; } = false;
+    }
+}
