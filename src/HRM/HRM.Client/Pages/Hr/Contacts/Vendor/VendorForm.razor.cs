938c8f95aa1e76cfd18925141e38edf507e1c8fd
hung.tran <hung.tran@saigontechnology.com>
2021-09-08 08:34:48 +0700

    refs #18. Implemnt vendor page

---
+++ src/HRM/HRM.Client/Pages/Hr/Contacts/Vendor/VendorForm.razor.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using HRM.Model;
+using HRM.Model.HR;
+using HRM.Client.Helpers;
+using HRM.Client.Models;
+using HRM.Client.Services;
+using HRM.Infrastructure;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace HRM.Client.Pages.Hr.Contacts.Vendor
+{
+    partial class VendorForm : ComponentBase
+    {
+        [Inject]
+        public ToastMessageHelper toastMessageHelper { get; set; }
+
+        [Inject]
+        public NavigationManager navigationManager { get; set; }
+
+        [Inject]
+        public IHttpClientService httpClientService { get; set; }
+
+        [Inject]
+        public SelectboxDataHelper selectboxDataHelper { get; set; }
+
+        [CascadingParameter(Name = "Bredcrumb")]
+        public List<BreadcurmbModel> Breadcrumb { get; set; } = new List<BreadcurmbModel>();
+
+        [Parameter]
+        public long? Id { get; set; }
+
+        private VendorModel model = new VendorModel();
+
+        private string pageTitle = "Thêm mới";
+        private bool pageLoading = true;
+
+        protected override async Task OnInitializedAsync()
+        {
+            Breadcrumb.Add(new BreadcurmbModel()
+            {
+                Title = "Quản lý nhân sự",
+                Href = "hr/contacts",
+                IsActive = false,
+            });
+
+            Breadcrumb.Add(new BreadcurmbModel()
+            {
+                Title = "Danh sách nhà cung cấp",
+                Href = "hr/contacts/vendor",
+                IsActive = false,
+            });
+
+            if (Id.HasValue)
+            {
+                pageTitle = "Cập nhật";
+
+                Breadcrumb.Add(new BreadcurmbModel()
+                {
+                    Title = "Cập nhật",
+                    IsActive = true,
+                });
+                StateHasChanged();
+
+                var result = await httpClientService.Get<VendorModel, HttpDataResponseWrapper<VendorModel>>($"vendor/{Id.Value}");
+
+                if (result != null)
+                {
+                    if (result.Succeeded == true)
+                    {
+                        model = result.Data;
+                    }
+                    else
+                    {
+                        await toastMessageHelper.Error(result.Message);
+                    }
+                }
+            }
+            else
+            {
+                Breadcrumb.Add(new BreadcurmbModel()
+                {
+                    Title = "Thêm mới",
+                    IsActive = true,
+                });
+                StateHasChanged();
+            }
+
+            pageLoading = false;
+            StateHasChanged();
+        }
+
+        protected void BackToTheListClick()
+        {
+            navigationManager.NavigateTo("hr/contacts/vendor");
+        }
+
+        protected async Task OnFinish(EditContext editContext)
+        {
+            if (!editContext.Validate())
+            {
+                return;
+            }
+
+            pageLoading = true;
+
+            if (Id.HasValue)
+            {
+                var response = await httpClientService.Put<VendorModel, HttpActionResponseWrapper>($"vendor/{Id.Value}", model);
+                if (response.Succeeded)
+                {
+                    await toastMessageHelper.UpdateSuccess();
+                    BackToTheListClick();
+                }
+                else
+                {
+                    await toastMessageHelper.Error(response.Message);
+                    pageLoading = false;
+                }
+            }
+            else
+            {
+                var response = await httpClientService.Post<VendorModel, HttpActionResponseWrapper>("vendor", model);
+                if (response.Succeeded)
+                {
+                    await toastMessageHelper.CreateSuccess();
+                    BackToTheListClick();
+                }
+                else
+                {
+                    await toastMessageHelper.Error(response.Message);
+                    pageLoading = false;
+                }
+            }
+            StateHasChanged();
+        }
+
+    }
+}
---
+++ src/HRM/HRM.Client/Pages/Hr/Contacts/Vendor/VendorList.razor.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Linq;
+using HRM.Model.HR;
+using HRM.Client.Models;
+using HRM.Infrastructure;
+using DotNetCore.Objects;
+using HRM.Client.Services;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using AntDesign;
+using HRM.Model;
+using HRM.Client.Helpers;
+
+namespace HRM.Client.Pages.Hr.Contacts.Vendor
+{
+    partial class VendorList : ComponentBase
+    {
+        [Inject]
+        public ToastMessageHelper toastMessageHelper { get; set; }
+
+        [Inject]
+        public NavigationManager navigationManager { get; set; }
+
+        [Inject]
+        public IHttpClientService httpClientService { get; set; }
+
+        [Inject]
+        public SelectboxDataHelper selectboxDataHelper { get; set; }
+
+        [CascadingParameter(Name = "Bredcrumb")]
+        public List<BreadcurmbModel> Breadcrumb { get; set; } = new List<BreadcurmbModel>();
+
+        private bool tableLoading = true;
+        private List<VendorGridModel> listData = new List<VendorGridModel>();
+        private int totalItems = 0;
+        Func<PaginationTotalContext, string> showTotal = ctr => $"Tổng: {ctr.Total} dòng";
+
+        private VendorGridParameterModel parameterModel = new VendorGridParameterModel();
+
+        private VendorGridModel deletedItem = new VendorGridModel();
+        private bool isVisibleDeleteModel = false;
+
+        protected async override Task OnInitializedAsync()
+        {
+            Breadcrumb.Add(new BreadcurmbModel()
+            {
+                Title = "Quản lý nhân sự",
+                Href = "hr/contacts",
+                IsActive = false,
+            });
+
+            Breadcrumb.Add(new BreadcurmbModel()
+            {
+                Title = "Danh sách nhà cung cấp",
+                IsActive = true,
+            });
+
+            await LoadGridData();
+        }
+
+        protected async Task SearchClick()
+        {
+            await LoadGridData();
+        }
+
+        protected async Task PageChange(PaginationEventArgs args)
+        {
+            if (totalItems == 0)
+            {
+                return;
+            }
+            parameterModel.Page = new Page() { Index = args.Page, Size = args.PageSize };
+            await LoadGridData();
+        }
+
+        protected void AddNewClick()
+        {
+            navigationManager.NavigateTo("vendor/create");
+        }
+
+        protected void UpdateClick(VendorGridModel item)
+        {
+            navigationManager.NavigateTo($"vendor/update/{item.Id}");
+        }
+
+        protected void DeleteClick(VendorGridModel item)
+        {
+            deletedItem = item;
+            isVisibleDeleteModel = true;
+        }
+
+        protected async Task AgreeDeleteClick()
+        {
+            var result = await httpClientService.Delete<VendorModel, HttpActionResponseWrapper>($"vendor/{deletedItem.Id}");
+            if (result.Succeeded)
+            {
+                await toastMessageHelper.DeleteSuccess();
+
+                if (listData.Count == 1)
+                {
+                    parameterModel.Page.Index = 1;
+                }
+
+                await LoadGridData();
+            }
+            else
+            {
+                await toastMessageHelper.Error(result.Message);
+            }
+            isVisibleDeleteModel = false;
+            deletedItem = null;
+        }
+
+        private async Task LoadGridData()
+        {
+            tableLoading = true;
+
+            var result = await httpClientService.Post<VendorGridParameterModel, HttpDataResponseWrapper<Model.Grid<VendorGridModel>>>("vendor/grid", parameterModel);
+            if (result != null)
+            {
+                listData = result.Data.List?.ToList() ?? new List<VendorGridModel>();
+                totalItems = (int)result.Data.Count;
+                tableLoading = false;
+            }
+            StateHasChanged();
+        }
+
+    }
+}
