fd2d18f7132cd89a16b60abb671c27472543fd56
hung.tran <hung.tran@saigontechnology.com>
2021-09-08 08:33:51 +0700

    refs #17 Implement Customer page

---
+++ src/HRM/HRM.Client/Pages/Hr/Contacts/Customer/CustomerForm.razor.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using HRM.Model;
+using HRM.Model.HR;
+using HRM.Client.Helpers;
+using HRM.Client.Models;
+using HRM.Client.Services;
+using HRM.Infrastructure;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace HRM.Client.Pages.Hr.Contacts.Customer
+{
+    partial class CustomerForm : ComponentBase
+    {
+        [Inject]
+        public ToastMessageHelper toastMessageHelper { get; set; }
+
+        [Inject]
+        public NavigationManager navigationManager { get; set; }
+
+        [Inject]
+        public IHttpClientService httpClientService { get; set; }
+
+        [Inject]
+        public SelectboxDataHelper selectboxDataHelper { get; set; }
+
+        [CascadingParameter(Name = "Bredcrumb")]
+        public List<BreadcurmbModel> Breadcrumb { get; set; } = new List<BreadcurmbModel>();
+
+        [Parameter]
+        public long? Id { get; set; }
+
+        private CustomerModel model = new CustomerModel();
+
+        private string pageTitle = "Thêm mới";
+        private bool pageLoading = true;
+
+        protected override async Task OnInitializedAsync()
+        {
+            Breadcrumb.Add(new BreadcurmbModel()
+            {
+                Title = "Quản lý nhân sự",
+                Href = "hr/contacts",
+                IsActive = false,
+            });
+
+            Breadcrumb.Add(new BreadcurmbModel()
+            {
+                Title = "Danh sách khách hàng",
+                Href = "hr/contacts/customer",
+                IsActive = false,
+            });
+
+            if (Id.HasValue)
+            {
+                pageTitle = "Cập nhật";
+
+                Breadcrumb.Add(new BreadcurmbModel()
+                {
+                    Title = "Cập nhật",
+                    IsActive = true,
+                });
+                StateHasChanged();
+
+                var result = await httpClientService.Get<CustomerModel, HttpDataResponseWrapper<CustomerModel>>($"customer/{Id.Value}");
+
+                if (result != null)
+                {
+                    if (result.Succeeded == true)
+                    {
+                        model = result.Data;
+                    }
+                    else
+                    {
+                        await toastMessageHelper.Error(result.Message);
+                    }
+                }
+            }
+            else
+            {
+                Breadcrumb.Add(new BreadcurmbModel()
+                {
+                    Title = "Thêm mới",
+                    IsActive = true,
+                });
+                StateHasChanged();
+            }
+
+            pageLoading = false;
+            StateHasChanged();
+        }
+
+        protected void BackToTheListClick()
+        {
+            navigationManager.NavigateTo("hr/contacts/Customer");
+        }
+
+        protected async Task OnFinish(EditContext editContext)
+        {
+            if (!editContext.Validate())
+            {
+                return;
+            }
+
+            pageLoading = true;
+
+            if (Id.HasValue)
+            {
+                var response = await httpClientService.Put<CustomerModel, HttpActionResponseWrapper>($"customer/{Id.Value}", model);
+                if (response.Succeeded)
+                {
+                    await toastMessageHelper.UpdateSuccess();
+                    BackToTheListClick();
+                }
+                else
+                {
+                    await toastMessageHelper.Error(response.Message);
+                    pageLoading = false;
+                }
+            }
+            else
+            {
+                var response = await httpClientService.Post<CustomerModel, HttpActionResponseWrapper>("customer", model);
+                if (response.Succeeded)
+                {
+                    await toastMessageHelper.CreateSuccess();
+                    BackToTheListClick();
+                }
+                else
+                {
+                    await toastMessageHelper.Error(response.Message);
+                    pageLoading = false;
+                }
+            }
+            StateHasChanged();
+        }
+
+        protected string FormatMoney(decimal value)
+        {
+            return value.ToString("n0");
+        }
+
+        protected string ParseMoney(string value)
+        {
+            return Regex.Replace(value, @"\$\s?|(,*)", "");
+        }
+
+    }
+}
---
+++ src/HRM/HRM.Client/Pages/Hr/Contacts/Customer/CustomerList.razor.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Linq;
+using HRM.Model.HR;
+using HRM.Client.Models;
+using HRM.Infrastructure;
+using DotNetCore.Objects;
+using HRM.Client.Services;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using AntDesign;
+using HRM.Model;
+using HRM.Client.Helpers;
+
+namespace HRM.Client.Pages.Hr.Contacts.Customer
+{
+    partial class CustomerList : ComponentBase
+    {
+        [Inject]
+        public ToastMessageHelper toastMessageHelper { get; set; }
+
+        [Inject]
+        public NavigationManager navigationManager { get; set; }
+
+        [Inject]
+        public IHttpClientService httpClientService { get; set; }
+
+        [Inject]
+        public SelectboxDataHelper selectboxDataHelper { get; set; }
+
+        [CascadingParameter(Name = "Bredcrumb")]
+        public List<BreadcurmbModel> Breadcrumb { get; set; } = new List<BreadcurmbModel>();
+
+        private bool tableLoading = true;
+        private List<CustomerGridModel> listData = new List<CustomerGridModel>();
+        private int totalItems = 0;
+        Func<PaginationTotalContext, string> showTotal = ctr => $"Tổng: {ctr.Total} dòng";
+
+        private CustomerGridParameterModel parameterModel = new CustomerGridParameterModel();
+
+        private CustomerGridModel deletedItem = new CustomerGridModel();
+        private bool isVisibleDeleteModel = false;
+
+        protected async override Task OnInitializedAsync()
+        {
+            Breadcrumb.Add(new BreadcurmbModel()
+            {
+                Title = "Quản lý nhân sự",
+                Href = "hr/contacts",
+                IsActive = false,
+            });
+
+            Breadcrumb.Add(new BreadcurmbModel()
+            {
+                Title = "Danh sách khách hàng",
+                IsActive = true,
+            });
+
+            await LoadGridData();
+        }
+
+        protected async Task SearchClick()
+        {
+            await LoadGridData();
+        }
+
+        protected async Task PageChange(PaginationEventArgs args)
+        {
+            if (totalItems == 0)
+            {
+                return;
+            }
+            parameterModel.Page = new Page() { Index = args.Page, Size = args.PageSize };
+            await LoadGridData();
+        }
+
+        protected void AddNewClick()
+        {
+            navigationManager.NavigateTo("customer/create");
+        }
+
+        protected void UpdateClick(CustomerGridModel item)
+        {
+            navigationManager.NavigateTo($"customer/update/{item.Id}");
+        }
+
+        protected void DeleteClick(CustomerGridModel item)
+        {
+            deletedItem = item;
+            isVisibleDeleteModel = true;
+        }
+
+        protected async Task AgreeDeleteClick()
+        {
+            var result = await httpClientService.Delete<CustomerModel, HttpActionResponseWrapper>($"customer/{deletedItem.Id}");
+            if (result.Succeeded)
+            {
+                await toastMessageHelper.DeleteSuccess();
+
+                if (listData.Count == 1)
+                {
+                    parameterModel.Page.Index = 1;
+                }
+
+                await LoadGridData();
+            }
+            else
+            {
+                await toastMessageHelper.Error(result.Message);
+            }
+            isVisibleDeleteModel = false;
+            deletedItem = null;
+        }
+
+        private async Task LoadGridData()
+        {
+            tableLoading = true;
+
+            var result = await httpClientService.Post<CustomerGridParameterModel, HttpDataResponseWrapper<Model.Grid<CustomerGridModel>>>("customer/grid", parameterModel);
+            if (result != null)
+            {
+                listData = result.Data.List?.ToList() ?? new List<CustomerGridModel>();
+                totalItems = (int)result.Data.Count;
+                tableLoading = false;
+            }
+            StateHasChanged();
+        }
+
+    }
+}
