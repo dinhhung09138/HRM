fb4b0a70ff9624a5fc057dce82aa7b47a99bf17e
hung.tran <hung.tran@saigontechnology.com>
2021-09-11 22:24:50 +0700

    refs #19, Layout design

---
+++ src/HRM/HRM.Client/Pages/Hr/Employee/EmployeeForm.razor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using HRM.Model.HR;
+using HRM.Client.Models;
+using HRM.Infrastructure;
+using DotNetCore.Objects;
+using HRM.Client.Services;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using AntDesign;
+using HRM.Client.Helpers;
+
+namespace HRM.Client.Pages.Hr.Employee
+{
+    partial class EmployeeForm : ComponentBase
+    {
+        [Inject]
+        public ToastMessageHelper toastMessageHelper { get; set; }
+
+        [Inject]
+        public NavigationManager navigationManager { get; set; }
+
+        [Inject]
+        public IHttpClientService httpClientService { get; set; }
+
+        [Inject]
+        public SelectboxDataHelper selectboxDataHelper { get; set; }
+
+        [CascadingParameter(Name = "Bredcrumb")]
+        public List<BreadcurmbModel> Breadcrumb { get; set; } = new List<BreadcurmbModel>();
+
+        [Parameter]
+        public long? Id { get; set; }
+
+    }
+}
